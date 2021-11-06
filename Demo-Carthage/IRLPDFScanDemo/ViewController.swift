ef43010cd75e1de0248b6e1f2370b56ffa6d1974
MARTIN Denis <denis@irlmobile.com>
2021-11-06 20:53:15 +0100

    Added project to support Carthage

---
+++ Demo-Carthage/IRLPDFScanDemo/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  IRLPDFScanDemo
+//
+//  Created by Denis Martin-Bruillot on 06/11/2021.
+//
+
+import UIKit
+import IRLPDFScanContent
+
+class ViewController: UIViewController {
+
+    let scanner = IRLPDFScanContent()
+    
+    @IBOutlet weak var text: UILabel!
+    
+    @IBOutlet weak var pdfView: PDFView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.bringSubviewToFront(text)
+    }
+
+    @IBAction func startScan(_ sender: Any) {
+        pdfView.document = nil
+        text.text = "Use the Scan Button"
+        scanner.delegate = self
+        scanner.present(animated: true, completion: nil)
+    }
+    
+}
+
+extension ViewController: IRLPDFScanContentProtocol {
+    
+    func scanContent(caller: IRLPDFScanContent, didScan scan: VNDocumentCameraScan) {
+        guard let url = caller.generatePDF() else {
+            return
+        }
+        view.sendSubviewToBack(text)
+        pdfView.document = PDFDocument(url: url)
+        pdfView.autoScales = true
+    }
+    
+    func scanContent(caller: IRLPDFScanContent, didFail error: Error) {
+        view.bringSubviewToFront(text)
+        text.text = error.localizedDescription
+    }
+    
+}
---
+++ IRLPDFScanContent/IRLPDFScanContent.h
@@ -0,0 +1,18 @@
+//
+//  IRLPDFScanContent.h
+//  IRLPDFScanContent
+//
+//  Created by Denis Martin-Bruillot on 06/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IRLPDFScanContent.
+FOUNDATION_EXPORT double IRLPDFScanContentVersionNumber;
+
+//! Project version string for IRLPDFScanContent.
+FOUNDATION_EXPORT const unsigned char IRLPDFScanContentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRLPDFScanContent/PublicHeader.h>
+
+
