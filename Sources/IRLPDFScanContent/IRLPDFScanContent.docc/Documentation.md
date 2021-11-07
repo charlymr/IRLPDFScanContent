e225927f2f36a4e2c1372ff2598ca91f3c048520
MARTIN Denis <denis@irlmobile.com>
2021-11-07 16:47:03 +0100

    - Migration tutorial

--- Sources/IRLPDFScanContent/IRLPDFScanContent.docc/Documentation.md
+++ Sources/IRLPDFScanContent/IRLPDFScanContent.docc/Documentation.md
@@ -21,6 +21,16 @@ A convenient class usable from `UIKit` view controller or `SwiftUI` to scan docu
 
 Check the documentation (Essentials / Getting Started) here: [Documentation](https://irlpdfscancontent.irlmobile.com/tutorials/tutorial-table-of-contents) for more details
 
+## Migration from IRLScannerViewController
+Migration from [IRLScannerViewController](https://github.com/charlymr/IRLDocumentScanner) is feasible. Please check the tutorial <doc:Migrate-from-IRLScannerViewController>
+
+#### First you will need to replace your dependency and `import`
+See <doc:GettingStarted> for the different options (SPM, Cocoapods, Carthage)
+
+#### You project should still work with depreciation warnings
+Follow the <doc:Migrate-from-IRLScannerViewController> tutorial to see how to beneficiate from the new system & remove the depreciation warnings.
+
+One quick note, the new system support multiple scan. In this tutorial we cover how to get 1 scan. It is very straigth forward to get eveything else, check the doc.
 
 ## Topics
 
@@ -31,6 +41,7 @@ Check the documentation (Essentials / Getting Started) here: [Documentation](htt
 - <doc:/tutorials/Tutorial-Table-of-Contents>
 - <doc:ClassicalDelegate>
 - <doc:SwiftUI>
+- <doc:Migrate-from-IRLScannerViewController>
 
 ### Classes
 
---
+++ Sources/IRLPDFScanContent/IRLPDFScanContent.docc/Resources/code-files/Migration-1.swift
@@ -0,0 +1,49 @@
+import UIKit
+import IRLDocumentScanner
+
+class ViewController : UIViewController, IRLScannerViewControllerDelegate {
+    
+    @IBOutlet weak var scanButton: UIButton!
+    @IBOutlet weak var imageView:  UIImageView!
+    
+    // MARK: User Actions
+
+    @IBAction func scan(_ sender: AnyObject) {
+        let scanner = IRLScannerViewController.standardCameraView(with: self)
+        scanner.showControls = true
+        scanner.showAutoFocusWhiteRectangle = true
+        present(scanner, animated: true, completion: nil)
+    }
+    
+    // MARK: IRLScannerViewControllerDelegate
+    
+    func pageSnapped(_ page_image: UIImage, from controller: IRLScannerViewController) {
+        controller.dismiss(animated: true) { () -> Void in
+            self.imageView.image = page_image
+        }
+    }
+        
+    func cameraViewWillUpdateTitleLabel(_ cameraView: IRLScannerViewController) -> String? {
+        
+        var text = ""
+        switch cameraView.cameraViewType {
+        case .normal:           text = text + "NORMAL"
+        case .blackAndWhite:    text = text + "B/W-FILTER"
+        case .ultraContrast:    text = text + "CONTRAST"
+        }
+        
+        switch cameraView.detectorType {
+        case .accuracy:         text = text + " | Accuracy"
+        case .performance:      text = text + " | Performance"
+        }
+        
+        return text
+    }
+
+    func didCancel(_ cameraView: IRLScannerViewController) {
+        cameraView.dismiss(animated: true){ ()-> Void in
+            NSLog("Cancel pressed");
+           }
+    }
+    
+}
